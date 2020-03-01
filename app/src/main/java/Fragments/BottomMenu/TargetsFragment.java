b0159353bfe7e83f75faadd46ffea51a7d390be3
Ovchinnikov Sergey <owchin.s@yandex.ru>
2020-03-01 23:14:58 +0300

    Добавлен FAB

--- app/src/main/java/Fragments/BottomMenu/TargetsFragment.java
+++ app/src/main/java/Fragments/BottomMenu/TargetsFragment.java
@@ -1,5 +1,6 @@
 package Fragments.BottomMenu;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.serg.fit.R;
 
 import java.util.Arrays;
@@ -22,11 +24,14 @@ import Utils.LinearItemDecoration;
 public class TargetsFragment extends Fragment {
     private RecyclerView recyclerView;
     private TargetAdapter adapter;
+    private FloatingActionButton fab;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_targets,null);
+        fab = (FloatingActionButton) view.findViewById(R.id.fab_btn);
+        fab.setColorFilter(Color.WHITE);
         recyclerView = (RecyclerView) view.findViewById(R.id.targets_recyclerView);
         adapter = new TargetAdapter();
         recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
@@ -38,6 +43,16 @@ public class TargetsFragment extends Fragment {
 
     private void loadItems() {
        adapter.addItems(Arrays.asList(new TargetItem(false,"Первая программа",4,50,22220,20,10)));
+        adapter.addItems(Arrays.asList(new TargetItem(false,"Первая программа",4,50,22220,20,10)));
+        adapter.addItems(Arrays.asList(new TargetItem(false,"Первая программа",4,50,22220,20,10)));
+        adapter.addItems(Arrays.asList(new TargetItem(false,"Первая программа",4,50,22220,20,10)));
+        adapter.addItems(Arrays.asList(new TargetItem(false,"Первая программа",4,50,22220,20,10)));
+        adapter.addItems(Arrays.asList(new TargetItem(false,"Первая программа",4,50,22220,20,10)));
+        adapter.addItems(Arrays.asList(new TargetItem(false,"Первая программа",4,50,22220,20,10)));
+        adapter.addItems(Arrays.asList(new TargetItem(false,"Первая программа",4,50,22220,20,10)));
+        adapter.addItems(Arrays.asList(new TargetItem(false,"Первая программа",4,50,22220,20,10)));
+        adapter.addItems(Arrays.asList(new TargetItem(false,"Первая программа",4,50,22220,20,10)));
+
     }
 }
 
